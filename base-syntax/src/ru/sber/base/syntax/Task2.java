68a5436e4fa5acb1d7ea3d15f5c58978715badb0
bezculturshina <144541430+bezculturshina@users.noreply.github.com>
2025-02-24 20:25:09 +0300

    Commit Message

--- base-syntax/src/ru/sber/base/syntax/Task2.java
+++ base-syntax/src/ru/sber/base/syntax/Task2.java
@@ -16,8 +16,5 @@ public class Task2 {
             }
 
         }
-
-
-
     }
 }
---
+++ base-syntax/src/ru/sber/base/syntax/Task3.java
@@ -0,0 +1,9 @@
+package ru.sber.base.syntax;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 3; i++) {
+            System.out.println("* * * * * * * * * *\n" + " * * * * * * * * *");
+        }
+    }
+}
