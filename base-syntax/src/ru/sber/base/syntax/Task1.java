aecce26394505f913ce98245018246ee5e5e7778
bezculturshina <144541430+bezculturshina@users.noreply.github.com>
2025-02-24 20:00:25 +0300

    Commit Message

--- base-syntax/src/ru/sber/base/syntax/Task1.java
+++ base-syntax/src/ru/sber/base/syntax/Task1.java
@@ -1,10 +1,13 @@
 package ru.sber.base.syntax;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите Ваше имя: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + "!");
 
     }
 }
