2e6340e010b0a3e07533d70a326df45d675a164a
bezculturshina <144541430+bezculturshina@users.noreply.github.com>
2025-02-24 20:01:18 +0300

    Commit Message

---
+++ base-syntax/src/ru/sber/base/syntax/Task2.java
@@ -0,0 +1,23 @@
+package ru.sber.base.syntax;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите количество чисел: ");
+        int q = scan.nextInt();
+        if (q < 1 || q > 500) {
+            System.out.println("Введено недопустимое число "+ q + ". Допустимый диапазон значений: 1 - 500");
+        }
+        else {
+            for (int i = 1; i <= q; i++) {
+                System.out.print((i * 7) + " ");
+            }
+
+        }
+
+
+
+    }
+}
